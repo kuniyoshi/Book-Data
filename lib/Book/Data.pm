d16e2a867e9e3ca565eac84bb3d2aab421d3aefe
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-03-19 19:30:13 +0900

    initial commit

---
+++ lib/Book/Data.pm
@@ -0,0 +1,32 @@
+package Book::Data;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Book::Data -
+
+=head1 SYNOPSIS
+
+  use Book::Data;
+
+=head1 DESCRIPTION
+
+Book::Data is
+
+=head1 AUTHOR
+
+kuniyoshi kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
