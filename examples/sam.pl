b10ad42fa31ea20bd9f66383c72c3fa6c963e1eb
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-03-20 15:44:00 +0900

    Add a example.

---
+++ examples/sam.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open ":utf8";
+use open ":std";
+use Business::ISBN;
+use Data::Dumper qw( Dumper );
+use lib "lib";
+use Book::Data;
+
+die usage()
+    unless @ARGV;
+
+my @isbns = map { Business::ISBN->new( $_ ) } <>;
+
+my $util = Book::Data->new;
+
+foreach my $isbn ( @isbns ) {
+    my $book = $util->find( { isbn => $isbn } )->next;
+    say Dumper $book;
+}
+
+exit;
+
+sub usage {
+    return "usage: $0 <isbn>\n";
+}
+
